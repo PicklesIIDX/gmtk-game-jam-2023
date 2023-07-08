22204bcf6db3721df7abb04b445e0e01b9547cf0
Cummings427 <Cummings427@gmail.com>
2023-07-08 01:47:08 -0400

    Intial movement

---
+++ gmtk-game-jam/Assets/Scripts/Movement.cs
@@ -0,0 +1,152 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float playerSpeed;
+    public Rigidbody2D rigidbody;
+    //Bravery meter
+    //scared - brave - chaos
+    private Vector2 movementDirection;
+    private Vector2 inputDirection;
+
+    public Camera topCamera;
+    Vector2 mousePos;
+
+    public bool legControl;
+    bool isMovementCorRunning = false;
+
+    bool isSwinging = false;
+    public bool isSword;
+    public ParticleSystem swordBurst;
+    public bool isSpear;
+    public ParticleSystem spearBurst;
+    public bool isAxe;
+    public ParticleSystem axeBurst;
+
+    public bool isSheild;
+
+    public bool isBomb;
+    public ParticleSystem bombBurst;
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputCommands();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void InputCommands()
+    {
+        mousePos = topCamera.ScreenToWorldPoint(Input.mousePosition);
+        inputDirection.x = Input.GetAxisRaw("Horizontal");
+        inputDirection.y = Input.GetAxisRaw("Vertical");
+
+        //Are we boots?
+        if (legControl)
+        {
+            movementDirection.x = inputDirection.x;
+            movementDirection.y = inputDirection.y;
+        }
+        else
+        {
+            if (!isMovementCorRunning)
+            {
+                StartCoroutine(RandomMovement());
+            }
+        }
+
+        //Are we a weapon?
+        Weapons();
+
+        //Are we a bomb?
+        BombBlow();
+    }
+
+    public void Weapons()
+    {
+        if (isSword)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                StartCoroutine(SwordBurstCor());
+            }
+        }
+
+        else if (isSpear)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                StartCoroutine(SpearBurstCor());
+            }
+        }
+        else if (isAxe)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                StartCoroutine(AxeBurstCor());
+            }
+        }
+        else if (isSheild)
+        {
+            //Need to use angle taken from inputDirection.x and y to get direction it should face or use animation
+        }
+    }
+
+    public void BombBlow()
+    {
+        if (isBomb)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                bombBurst.Emit(100);
+            }
+        }
+    }
+
+    IEnumerator SwordBurstCor()
+    {
+        //Need to use angle taken from inputDirection.x and y to get direction it should face or use animation
+        swordBurst.Emit(100);
+        yield return new WaitForSeconds(1.5f);
+    }
+
+    IEnumerator SpearBurstCor()
+    {
+        //Need to use angle taken from inputDirection.x and y to get direction it should face or use animation
+        spearBurst.Emit(100);
+        yield return new WaitForSeconds(1);
+    }
+
+    IEnumerator AxeBurstCor()
+    {
+        //Need to use angle taken from inputDirection.x and y to get direction it should face or use animation
+        axeBurst.Emit(100);
+        yield return new WaitForSeconds(2);
+    }
+
+
+    IEnumerator RandomMovement()
+    {
+        //Needs general direction it should strive for with some tossed in chaos
+        isMovementCorRunning = true;
+        movementDirection.x = Random.Range(-1.0f, 1.0f);
+        movementDirection.y = Random.Range(-1.0f, 1.0f);
+        yield return new WaitForSeconds(1);
+        isMovementCorRunning = false;
+    }
+
+    void Move()
+    {
+        rigidbody.MovePosition(rigidbody.position + movementDirection.normalized * playerSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDirection = mousePos - rigidbody.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rigidbody.rotation = angle;
+    }
+}
