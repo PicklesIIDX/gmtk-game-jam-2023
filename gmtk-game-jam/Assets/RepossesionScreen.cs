32710fb4ba1816aaf2c80570175930a015fbf3df
auston <auston.montville@gmail.com>
2023-07-07 23:50:54 -0700

    - crude repossesion loop

--- gmtk-game-jam/Assets/RepossesionScreen.cs
+++ gmtk-game-jam/Assets/RepossesionScreen.cs
@@ -7,16 +7,23 @@ public class RepossesionScreen : MonoBehaviour
 {
 
     [SerializeField] private UIDocument document;
+
+    [SerializeField] private GameObject newHero;
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
         var button = document.rootVisualElement.Q<Button>("item1");
-        button?.RegisterCallback<ClickEvent>(_ =>
-        {
-            Debug.Log("selected item 1");
-            document.enabled = false;
-        });
-        document.enabled = false;
+        button.RegisterCallback<ClickEvent>(Close);
+        button = document.rootVisualElement.Q<Button>("item2");
+        button.RegisterCallback<ClickEvent>(Close);
+    }
+
+    private void Close(ClickEvent _)
+    {
+        Debug.Log("selected item 1");
+        var newHeroInstance = GameObject.Instantiate(newHero);
+        newHeroInstance.transform.position = new Vector3(0, 0);
+        gameObject.SetActive(false);
     }
 
     // Update is called once per frame
