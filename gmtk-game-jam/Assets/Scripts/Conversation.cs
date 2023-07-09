7aa076abf6e27bd63ec56c3354f293eef209f330
auston <auston.montville@gmail.com>
2023-07-08 22:10:02 -0700

    - chat structure and some other experiments

---
+++ gmtk-game-jam/Assets/Scripts/Conversation.cs
@@ -0,0 +1,16 @@
+﻿
+	using System;
+	using UnityEngine;
+
+	[Serializable]
+	public struct ChatEntry
+	{
+		public string Speaker;
+		public string Line;
+	}
+	
+	[CreateAssetMenu(fileName = "conversation-01", menuName = "Conversation", order = 0)]
+	public class Conversation : ScriptableObject
+	{
+		[SerializeField] public ChatEntry[] ChatEntries;
+	}
