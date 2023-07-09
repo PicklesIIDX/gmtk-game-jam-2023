1e745594fc7d8e281f4c6454e1f728f8f35d8c13
mhanapole <mhanapole@vt.edu>
2023-07-08 18:41:34 -0700

    Added URP, added palette swap shader

---
+++ gmtk-game-jam/Assets/HeroPaletteSwap.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class HeroPaletteSwap : MonoBehaviour
+{
+    [Serializable]
+    public class HeroPalette
+    {
+        public Color OutlineColor = Color.black;
+        public Color SkinColor;
+        public Color TunicMainColor;
+        public Color TunicAccentColor;
+        public Color BootColor;
+        public Color HairColor;
+        public Color EyeWhiteColor = Color.white;
+        public Color EyeColor;
+    }
+
+    public SpriteRenderer Sprite;
+
+    public List<HeroPalette> AcceptablePalettes;
+    public int SelectedPalette;
+    public bool next;
+
+    private void Update()
+    {
+        if (next)
+        {
+            next = false;
+            SelectedPalette = (SelectedPalette + 1) % AcceptablePalettes.Count;
+            SetPalette(SelectedPalette);
+        }
+    }
+
+    private void Start()
+    {
+        RandomizePalette();
+    }
+
+    [ContextMenu("Randomize Palette")]
+    private void RandomizePalette()
+    {
+        if (AcceptablePalettes.Count == 0) return;
+        SelectedPalette = Random.Range(0, AcceptablePalettes.Count);
+        SetPalette(SelectedPalette);
+    }
+
+    [ContextMenu("Set Palette")]
+    private void SetPaletteToSelected()
+    {
+        SetPalette(SelectedPalette);
+    }
+    
+    private void SetPalette(int index)
+    {
+        if (index < 0 || index >= AcceptablePalettes.Count) return;
+        
+        HeroPalette palette = AcceptablePalettes[index];
+        Sprite.material.SetColor("_OutlineColor", palette.OutlineColor);
+        Sprite.material.SetColor("_SkinColor", palette.SkinColor);
+        Sprite.material.SetColor("_TunicMainColor", palette.TunicMainColor);
+        Sprite.material.SetColor("_TunicAccentColor", palette.TunicAccentColor);
+        Sprite.material.SetColor("_BootColor", palette.BootColor);
+        Sprite.material.SetColor("_HairColor", palette.HairColor);
+        Sprite.material.SetColor("_EyeWhiteColor", palette.EyeWhiteColor);
+        Sprite.material.SetColor("_EyeColor", palette.EyeColor);
+    }
+}
